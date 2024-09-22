5f8e49434246c6693f98b0cbe9445ffd866d916b
pranesh <praneshv37@gmail.com>
2024-09-21 21:37:46 -0400

    Initial Commit

---
+++ Spacecrafts AR2/Spacecrafts_AR2App.swift
@@ -0,0 +1,17 @@
+//
+//  Spacecrafts_AR2App.swift
+//  Spacecrafts AR2
+//
+//  Created by Pranesh Velmurugan on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Spacecrafts_AR2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spacecrafts AR2Tests/Spacecrafts_AR2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Spacecrafts_AR2Tests.swift
+//  Spacecrafts AR2Tests
+//
+//  Created by Pranesh Velmurugan on 9/21/24.
+//
+
+import Testing
+@testable import Spacecrafts_AR2
+
+struct Spacecrafts_AR2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
